895f688640fc6c92e6bb30a6087aabd8933af18e
kareem <kareemgomaaa98@gmail.com>
2023-08-05 18:25:49 +0300

    1st commit

--- Terraform/1_backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "cicd-project-backend-storage-file"
-    key    = "terraform_backend/terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "backend"
-  }
-}
