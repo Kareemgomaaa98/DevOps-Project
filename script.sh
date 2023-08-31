e1d16d572e4d1c11c02cfd3268273c81bee9359e
kareem <kareemgomaaa98@gmail.com>
2023-08-31 15:01:16 +0300

    edits

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Install ArgoCD
+# Install ArgoCD and configure the project
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl port-forward svc/argocd-server -n argocd 8080:443
+kubectl apply -f application.yaml
