503df0c506e00026e0b1435f872f3d452e159d3d
kareem <kareemgomaaa98@gmail.com>
2023-09-02 17:44:07 +0300

    added sleep command

--- script.sh
+++ script.sh
@@ -3,5 +3,6 @@
 # Install ArgoCD and configure the project
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sleep 300
 kubectl port-forward svc/argocd-server -n argocd 8080:443
 kubectl apply -f application.yaml
