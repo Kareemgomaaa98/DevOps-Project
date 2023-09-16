e1bccc42e198bae3613154a13c3e4842290f2753
kareem <kareemgomaaa98@gmail.com>
2023-09-16 15:28:54 +0300

    uploaded

--- automate.py
+++ automate.py
@@ -1,11 +1,12 @@
 import subprocess
-
+#python3 automate.py | tee >(ts '%Y-%m-%d %H:%M:%S' >> project.log)
 def main():
     print("\n$$ Welcome to my CI/CD Project $$\n--- Please choose what to do ?")
     user = input("1- terraform apply\n2- terraform destroy\n3- ansible\n4- Link me to my cluster\n5- ArgoCD\n9- exit\nEnter choice:")
 
-    if user == "1" or user == "terraform apply":
+    if user == "1" or user == "terraform apply and start ansible":
         terraform_apply()
+        ansible()
         main()
 
     elif user == "2" or user == "terraform destroy":
@@ -26,7 +27,7 @@ def main():
         main()
 
     elif user == "9" or user == "exit":
-        exit
+        exit()
 
     else:
         print("Please enter a correct choice")
@@ -45,22 +46,23 @@ def terraform_destroy():
     subprocess.run(["rm", "inventory.txt", "TFkey",], cwd=ansible_files) #Remove old inventory and key pair files
     
 def ansible():
-    print("\n Ansible starts now :) \n \n Ansible takes 2 or 3 minutes \n Please wait ... ")  
-    subprocess.run(["chmod", "400", "TFkey"], cwd=ansible_files) #Set the key pair file permission to read only
+    print("\n Ansible starts now :) \n \n Ansible takes 2 or 3 minutes \n Please wait ... ")
+    subprocess.run(["chmod", "400", "TFkey"], cwd=ansible_files)  # Set the key pair file permission to read only
     commands = [
-        ["ansible-playbook", "-i", "inventory.txt", "jenkins_instance.yml", "-u", "ubuntu", "--key-file", "TFkey", ">>", "playbooks_output_logs/jenkins_instance_.log"],
-        ["ansible-playbook", "-i", "inventory.txt", "sonarqube_instance.yml", "-u", "ubuntu", "--key-file", "TFkey", ">>", "playbooks_output_logs/sonarqube_playbook_output.log"],
-        ["ansible-playbook", "-i", "inventory.txt", "nexus_instance.yml", "-u", "ubuntu", "--key-file", "TFkey", ">>", "playbooks_output_logs/nexus_output.log"]
+        ["ansible-playbook", "-i", "inventory.txt", "jenkins_instance.yml", "-u", "ubuntu", "--key-file", "TFkey"] ,
+        ["ansible-playbook", "-i", "inventory.txt", "sonarqube_instance.yml", "-u", "ubuntu", "--key-file", "TFkey"],
+        ["ansible-playbook", "-i", "inventory.txt", "nexus_instance.yml", "-u", "ubuntu", "--key-file", "TFkey" ]
     ]
+
     # Create a list of terminal commands to run each command in a new terminal window
     terminal_commands = []
     for command in commands:
         terminal_commands.append(f"gnome-terminal --tab -- bash -c \"cd {ansible_files} && {' '.join(command)}; exec bash\"")
-        
+
     # Run each command in a new tab in the terminal window
     for terminal_command in terminal_commands:
-        subprocess.Popen(terminal_command, shell=True) 
-    
+        subprocess.Popen(terminal_command, shell=True)
+
 def kube():
     print("\n Linking to k8s :) \n ")  
     subprocess.run(["aws", "eks" , "--region", "us-east-1" , "update-kubeconfig" , "--name" , "kareem-cluster"])
