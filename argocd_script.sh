f045cc652c7767c938412ec907593a3277b4f681
kareem <kareemgomaaa98@gmail.com>
2023-10-15 09:06:42 +0300

    argocd repo update

--- argocd_script.sh
+++ argocd_script.sh
@@ -4,4 +4,4 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # sleep 300
-kubectl apply -f application.yaml
+kubectl apply -f argocd.yaml
